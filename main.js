2b560af519cece14241136a415fbb20587e62ddf
my-rooftop <dgwogh@dgist.ac.kr>
2021-08-07 20:12:23 +0900

    update sql version

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
+var mysql = require('mysql');
+
+var DB = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'tatamo4532',
+    database:'friends'
+});
+DB.connect();
+
 
 
 var app = http.createServer(function(request, response){
@@ -14,24 +24,43 @@ var app = http.createServer(function(request, response){
 
     if(pathname === '/'){
         if(queryData.id === undefined || queryData.id === 'home' ){
-            fs.readdir(`./data`, 'utf8', function(err, filelist){
+            DB.query(`SELECT * FROM introduce`, function(error, introduces){
+                if(error){
+                    throw error;
+                }
                 var title = 'Welcome';
                 var description = 'Hello, Welcome to my web page ^-^';
-                var list = template.List(filelist);
-                var html = template.HTML_home(title, list, `<h2>${title}</h2>${description}`, `<strong><a href="/create_friend">친구추가</a></strong>`);
+                var list = template.List(introduces);
+                var html = template.HTML_home(title, 
+                    list, 
+                    `<h2>${title}</h2>${description}`, 
+                    `<strong><a href="/create">친구추가</a></strong>`);
                 response.writeHead(200);
                 response.end(html);
             });
         } else {
-            fs.readdir(`./data`, 'utf8', function(err, filelist){
-                var title = queryData.id;
-                var list = template.List(filelist);
-                fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-                    var html = template.HTML_friend(title, list, `<h2>${title}</h2>${description}`, `<strong><a href="/create_guestbook">방명록남기기</a></strong> <strong><a href="/update?id=${title}">소개수정하기</a></strong> 
-                    <form action="delete_process" method = "post" > 
-                        <input type="hidden" name="id" value="${title}">
-                        <input type="submit" value="delete">
-                    </form>`);
+            DB.query(`SELECT * FROM introduce`, function(error, introduces){
+                if(error){
+                    throw error;
+                }
+                DB.query(`SELECT * FROM introduce WHERE id=?`, [queryData.id],function(error2, introduce){
+                    if(error2){
+                        throw error2;
+                    }
+                    console.log(introduce);
+                    var title = introduce[0].name;
+                    var description = introduce[0].description;
+                    var list = template.List(introduces);
+                    var html = template.HTML_friend(title, 
+                        list, 
+                        `<h2>${title}</h2>${description}`, 
+                        `
+                        <strong><a href="/create_guestbook">방명록남기기</a></strong>
+                        <strong><a href="/update?id=${queryData.id}">수정하기</a></strong>
+                        <form action="delete_process" method="post">
+                            <input type="hidden" name="id" value="${queryData.id}">
+                            <input type="submit" value="delete">
+                        </form>`);
                     response.writeHead(200);
                     response.end(html);
                 });
@@ -44,36 +73,37 @@ var app = http.createServer(function(request, response){
         });
         request.on('end', function(){
             var post = qs.parse(body);
-            var id = post.id;
-            fs.unlink(`data/${id}`, function(){
-                response.writeHead(302, {Location : `/`});
+            DB.query(`DELETE FROM introduce WHERE id = ?`, [post.id], function(error, result){
+                if(error){
+                    throw error;
+                }
+                response.writeHead(302, {Location: `/`});
                 response.end();
             });
         });
     }else if(pathname === '/update'){
-        fs.readdir('./data', function(error, filelist){
-            fs.readFile(`./data/${queryData.id}`, 'utf8', function(err, introduce){
-              var name = queryData.id;
-              var list = template.List(filelist);
-              var html = template.HTML_friend(name, list,
-                `
-                <form action="/update_process" method="post">
-                  <input type="hidden" name="id" value="${name}">
-                  <p><input type="text" name="name" placeholder="name" value="${name}"></p>
-                  <p>
-                    <textarea name="introduce" placeholder="introduce">${introduce}</textarea>
-                  </p>
-                  <p>
-                    <input type="submit">
-                  </p>
-                </form>
-                `,
-                ``
-              );
-              response.writeHead(200);
-              response.end(html);
+        DB.query(`SELECT * FROM introduce`, function(error, introduces){
+            if(error){
+                throw error;
+            }
+            DB.query(`SELECT * FROM introduce WHERE id = ?`, [queryData.id], function(error2, introduce){
+                if(error2){
+                    throw error2;
+                }
+                var title = 'Update friend information';
+                var list = template.List(introduces);
+                var html = template.HTML_friend(title, list,
+                    `<form action="/update_process" method="post">
+                    <input type="hidden" name="id" value="${introduce[0].id}">
+                    <p><input type="text" name="name" placeholder="name" value="${introduce[0].name}"></p>
+                    <p><textarea name="description" placeholder="description">${introduce[0].description}</textarea></p>
+                    <p><input type="submit"></p>
+                    </form>`,
+                    `<strong><a href="/create">친구추가</a></strong>`);
+                response.writeHead(200);
+                response.end(html);
             });
-          });
+        });
     } else if(pathname ==='/update_process'){
         var body = '';
         request.on('data', function(data){
@@ -81,55 +111,70 @@ var app = http.createServer(function(request, response){
         });
         request.on('end', function(){
             var post = qs.parse(body);
-            var id = post.id;
-            var name = post.name;
-            var introduce = post.introduce;
-            fs.rename(`data/${id}`, `data/${name}`, function(error){
-                fs.writeFile(`data/${name}`, introduce, 'utf8', function(err){
-                  response.writeHead(302, {Location: `/?id=${name}`});
-                  response.end();
-                })
-              });
+            DB.query(`UPDATE introduce SET name = ?, description = ?, author_id = 1 WHERE id = ?`,[post.name, post.description, post.id], function(error, result){
+                if(error){
+                    throw error;
+                }
+                response.writeHead(302, {Location: `/?id=${post.id}`});
+                response.end();
+            });
         });
-    } else if(pathname === '/create_friend'){
-        fs.readdir('./data', function(error, filelist){
-            var title = 'WEB - create';
-            var list = template.List(filelist);
-            var html = template.HTML_home(title, list, `
-                <form action ="http://localhost:3000/create_friend_process" method="post">
-                    친구의 이름과 소개글을 작성해주세요.
-                    <p><input type="text" name = "name" placeholder="name"></p>
-                    <p>
-                        <textarea name="introduce" placeholder="introduce"></textarea>
-                    </p> 
-                    <p>
-                        <input type="submit">
-                    </p>
-                </form>
-            `, ``);
+    } else if(pathname === '/create'){
+        // fs.readdir('./data', function(error, filelist){
+        //     var title = 'WEB - create';
+        //     var list = template.List(filelist);
+        //     var html = template.HTML_home(title, list, `
+        //         <form action ="/create_process" method="post">
+        //             친구의 이름과 소개글을 작성해주세요.
+        //             <p><input type="text" name = "name" placeholder="name"></p>
+        //             <p>
+        //                 <textarea name="introduce" placeholder="introduce"></textarea>
+        //             </p> 
+        //             <p>
+        //                 <input type="submit">
+        //             </p>
+        //         </form>
+        //     `, ``);
+        //     response.writeHead(200);
+        //     response.end(html);
+        // });
+        DB.query(`SELECT * FROM introduce`, function(error, introduces){
+            var title = 'Add friend';
+            var list = template.List(introduces);
+            var html = template.HTML_home(title, list,
+                `<form action="/create_process" method="post">
+                 <p><input type="text" name="name" placeholder="name"></p>
+                 <p><textarea name="description" placeholder="description"></textarea></p>
+                 <p><input type="submit"></p>
+                 </form>`,
+                 `<strong><a href="/create">친구추가</a></strong>`);
             response.writeHead(200);
             response.end(html);
         });
-    } else if( pathname === '/create_friend_process') {
+    } else if( pathname === '/create_process') {
         var body = '';
         request.on('data', function(data){
             body = body + data;
         });
         request.on('end', function(){
             var post = qs.parse(body);
-            var name = post.name;
-            var introduce = post.introduce ;
-            
-            fs.writeFile(`./data/${name}`, introduce, 'utf8', function(err){
-                response.writeHead(302, {Location: `/?id=${name}`});
+            // console.log(post);
+            DB.query(`INSERT INTO introduce (name, description, created, author_id) VALUES(?, ?, NOW(), ?)`,[post.name, post.description, 1], function(error, result){
+                if(error){
+                    throw error;
+                }
+                response.writeHead(302, {Location: `/?id=${result.insertId}`});
                 response.end();
             });
         });
+    } else if(pathname === '/create_guestbook"') {
+        //방명록 기능은 mysql 추가후에 ㄲ
     } else {
+        console.log('passpoint1');
         response.writeHead(404);
         response.end('Not found');
     }
 
 });
 
-app.listen(3000);
+app.listen(5000);
